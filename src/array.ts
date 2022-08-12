a0fa2ae37728851c7746c62e3e8c59a11163ab4d
fantasim <siuol@gmx.com>
2022-08-12 12:40:44 +0300

    ---

--- src/array.ts
+++
@@ -1,7 +0,0 @@
-export const NewIntArrayFilled = (length: number, from: number): number[] => {
-    let ret: number[] = []
-    for (let i =0; i < length; i++){
-        ret[i] = from + i
-    }
-    return ret
-}
