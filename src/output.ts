b0f2d09680c24cc113b5cacec81feeb145de218d
fantasim <siuol@gmx.com>
2022-08-31 08:54:07 +0300

    better output utils

--- src/output.ts
+++ src/output.ts
@@ -1,20 +1,9 @@
 import { InvBigInt } from "./involvera-types"
 
-export const CalculateOutputValueFromMelted = (meltedAmount: number, meltedRatio: number): InvBigInt => {
-    return new InvBigInt(Math.ceil(meltedAmount / meltedRatio))
+export const CalculateOutputValueFromMelted = (meltedAmount: InvBigInt, meltedRatio: number): InvBigInt => {
+    return InvBigInt.ceil(meltedAmount.divDecimals(meltedRatio))
 }
 
-export const CalculateOutputMeltedValue = (amount: BigInt, meltedRatio: number): InvBigInt => {
-    /* 
-        The value owned by an UTXO can excess the 2^53 limit of JS for precision, but once divided by 2 it can't.
-        So we divide the value by 2 to get the right precision, because the maximum value for an output value is between 2^53 & 2^54.
-        Then we add it back at the end because the maximum value for a melted output is < 2^53
-    */
-    const DIVIDER = 2
-    const safeNumberValueInt = BigInt(amount as any) / BigInt(DIVIDER as any)
-    const nStr = amount.toString()
-    const isLastNumberOdd = parseInt(nStr[nStr.length-1]) % 2 == 1
-    const rest = isLastNumberOdd ? (1 * meltedRatio) : 0
-
-    return new InvBigInt(Math.floor(((Number(safeNumberValueInt) * meltedRatio) * DIVIDER) + rest))
+export const CalculateOutputMeltedValue = (amount: InvBigInt, meltedRatio: number): InvBigInt => {
+    return new InvBigInt(amount.mulDecimals(meltedRatio))
 }
